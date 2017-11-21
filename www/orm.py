e9b9091d4bb16c5c57ad02f6d557c56d2441a76f
657994434 <657994434@qq.com>
2017-11-21 16:33:28 +0800

    orm

--- www/orm.py
+++ www/orm.py
@@ -17,15 +17,22 @@ async def create_pool(loop, **kw):
     __pool = await aiomysql.create_pool(
             host=kw.get('host', 'localhost'),
             port=kw.get('port', 3306),
-            user=kw['root'],
-            password=kw['lizhi'],
-            db=kw['python_blog'],
+            user=kw['user'],
+            password=kw['password'],
+            db=kw['db'],
             charset=kw.get('charset', 'utf8'),
             autocommit=kw.get('autocommit', True),
             maxsize=kw.get('maxsize', 10),
             minsize=kw.get('minsize', 1),
             loop=loop
     )
+
+
+async def destory_pool():
+    global __pool
+    if __pool is not None:
+        __pool.close()
+        await __pool.wait_closed()
 
 
 async def select(sql, args, size=None):
