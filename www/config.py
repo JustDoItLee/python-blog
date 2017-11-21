01c42185deafb5d18411bb2594b18321862598d6
657994434 <657994434@qq.com>
2017-11-21 16:53:20 +0800

    web配置

--- www/config.py
+++ www/config.py
@@ -9,10 +9,12 @@ __author__ = 'lz'
 
 import www.config_default
 
+
 class Dict(dict):
     '''
     Simple dict but support access as x.y style.
     '''
+
     def __init__(self, names=(), values=(), **kw):
         super(Dict, self).__init__(**kw)
         for k, v in zip(names, values):
@@ -27,6 +29,7 @@ class Dict(dict):
     def __setattr__(self, key, value):
         self[key] = value
 
+
 def merge(defaults, override):
     r = {}
     for k, v in defaults.items():
@@ -39,16 +42,19 @@ def merge(defaults, override):
             r[k] = v
     return r
 
+
 def toDict(d):
     D = Dict()
     for k, v in d.items():
         D[k] = toDict(v) if isinstance(v, dict) else v
     return D
 
+
 configs = www.config_default.configs
 
 try:
     import www.config_override
+
     configs = merge(configs, www.config_override.configs)
 except ImportError:
     pass
